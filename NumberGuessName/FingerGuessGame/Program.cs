0579c38f4497764e52c22f180e831d3bbd95120a
PengTiger <pengpofly@gmail.com>
2015-06-17 15:34:07 +0800

    HomeWork

---
+++ NumberGuessName/FingerGuessGame/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FingerGuessGame
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random FingerGuess = new Random();
+            string Player, Paper, Scissors, Stone;
+            Player = "";
+            Paper = "";
+            Scissors = "";
+            Stone = "";
+            Console.Write("請輸入要幾個人猜拳：");
+            int People = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= People; i++)
+            {
+                int j = FingerGuess.Next(1, 4);
+                switch (j)
+                {
+                    case 1:
+                        Player += "剪刀：" + i.ToString() + ",";
+                        Scissors += i.ToString() + ","; ;
+                        break;
+
+                    case 2:
+                        Player += "石頭：" + i.ToString() + ",";
+                        Stone += i.ToString() + ",";
+                        break;
+
+                    case 3:
+                        Player += "布：" + i.ToString() + ",";
+                        Paper += i.ToString() + ",";
+                        break;
+                }
+            }
+            Console.WriteLine("猜拳明細：" + Player);
+            if (Paper == "")
+                Console.WriteLine("此盤為石頭獲勝，獲勝者為：" + Stone);
+            else if (Scissors == "")
+                Console.WriteLine("此盤為布獲勝，獲勝者為：" + Paper);
+            else if (Stone == "")
+                Console.WriteLine("此盤為剪刀獲勝，獲勝者為：" + Scissors);
+            else
+                Console.WriteLine("此盤合局！！！");
+        }
+    }
+}
---
+++ NumberGuessName/GuessNumberGame/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessNumberGame
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            string a = "", b = "", c = "", d = "", Num = "";
+            for (int i = 1; i <= 4; i++)
+            {
+                if (a == "")
+                    a = rnd.Next(0, 10).ToString();
+                if (b == "")
+                {
+                    b = rnd.Next(0, 10).ToString();
+                    while (b == a)
+                        b = rnd.Next(0, 10).ToString();
+                }
+                if (c == "")
+                {
+                    c = rnd.Next(0, 10).ToString();
+                    while (c == a || c == b)
+                        c = rnd.Next(0, 10).ToString();
+                }
+                if (d == "")
+                {
+                    d = rnd.Next(0, 10).ToString();
+                    while (d == a || d == b || d == c)
+                        d = rnd.Next(0, 10).ToString();
+                }
+            }
+            Num = a + b + c + d;
+            Console.WriteLine("答案：" + Num);
+            string GuessNum = "", Answers = "";
+            while (GuessNum == "")
+            {
+                Console.Write("請輸入你要猜的四位數字：");
+                GuessNum = Console.ReadLine();
+                int SetCount = 0;             //位置A
+                int NumberCount = 0;          //數字B
+                if (int.Parse(a) == int.Parse(GuessNum) / 1000)
+                    SetCount += 1;
+                else if (int.Parse(a) == int.Parse(GuessNum) / 100 - (int.Parse(GuessNum) / 1000) * 10)
+                    NumberCount += 1;
+                else if (int.Parse(a) == (int.Parse(GuessNum) / 10) % 10)
+                    NumberCount += 1;
+                else if (int.Parse(a) == int.Parse(GuessNum) % 10)
+                    NumberCount += 1;
+                if (int.Parse(b) == int.Parse(GuessNum) / 1000)
+                    NumberCount += 1;
+                else if (int.Parse(b) == int.Parse(GuessNum) / 100 - (int.Parse(GuessNum) / 1000) * 10)
+                    SetCount += 1;
+                else if (int.Parse(b) == (int.Parse(GuessNum) / 10) % 10)
+                    NumberCount += 1;
+                else if (int.Parse(b) == int.Parse(GuessNum) % 10)
+                    NumberCount += 1;
+                if (int.Parse(c) == int.Parse(GuessNum) / 1000)
+                    NumberCount += 1;
+                else if (int.Parse(c) == int.Parse(GuessNum) / 100 - (int.Parse(GuessNum) / 1000) * 10)
+                    NumberCount += 1;
+                else if (int.Parse(c) == (int.Parse(GuessNum) / 10) % 10)
+                    SetCount += 1;
+                else if (int.Parse(c) == int.Parse(GuessNum) % 10)
+                    NumberCount += 1;
+                if (int.Parse(d) == int.Parse(GuessNum) / 1000)
+                    NumberCount += 1;
+                else if (int.Parse(d) == int.Parse(GuessNum) / 100 - (int.Parse(GuessNum) / 1000) * 10)
+                    NumberCount += 1;
+                else if (int.Parse(d) == (int.Parse(GuessNum) / 10) % 10)
+                    NumberCount += 1;
+                else if (int.Parse(d) == int.Parse(GuessNum) % 10)
+                    SetCount += 1;
+                GuessNum = "";
+                Answers = SetCount + "A" + NumberCount + "B";
+                Console.WriteLine("你猜的數字有：" + Answers);
+                if (Answers == "4A0B")
+                {
+                    Console.WriteLine("恭喜你猜中數字：" + Num);
+                    break;
+                }
+            }
+        }
+    }
+}
